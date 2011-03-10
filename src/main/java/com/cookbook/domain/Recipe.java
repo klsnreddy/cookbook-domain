e53a62d59d5f293869e909d3455fe2e702b4fc4b
akkinenivijay <akkineni.vijay@gmail.com>
2011-03-10 02:04:31 -0500

    Initial Commit for domain objects
    Signed-off-by: akkinenivijay <akkineni.vijay@gmail.com>

---
+++ src/main/java/com/cookbook/domain/Recipe.java
@@ -0,0 +1,131 @@
+/**
+ * 
+ */
+package com.cookbook.domain;
+
+import java.io.Serializable;
+
+/**
+ * @author lokesh
+ * 
+ */
+public class Recipe implements Serializable {
+
+	private static final long serialVersionUID = -8333501449975018182L;
+
+	private long id;
+
+	private String title;
+
+	private String contents;
+
+	private String author;
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @param title
+	 *            the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * @return the contents
+	 */
+	public String getContents() {
+		return contents;
+	}
+
+	/**
+	 * @param contents
+	 *            the contents to set
+	 */
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	/**
+	 * @return the author
+	 */
+	public String getAuthor() {
+		return author;
+	}
+
+	/**
+	 * @param author
+	 *            the author to set
+	 */
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Recipe [id=" + id + ", title=" + title + ", contents="
+				+ contents + ", author=" + author + "]";
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Recipe)) {
+			return false;
+		}
+		Recipe other = (Recipe) obj;
+		if (id != other.id) {
+			return false;
+		}
+		return true;
+	}
+
+}
